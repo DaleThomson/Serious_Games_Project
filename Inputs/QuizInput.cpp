61fafc0f422c1ae3cbfa9ce1a3efa306ecd10972
dale2707 <dalethomson2707@gmail.com>
2021-07-12 19:41:28 +0100

    Fixed Duplicate Questions Appearing

--- Inputs/QuizInput.cpp
+++ Inputs/QuizInput.cpp
@@ -23,7 +23,12 @@ void QuizInput::questionHandler()
 		std::ofstream reloadFile("../Resources/Quiz/QuizBuffer.txt");
 		while (getline(questionBackup, line))
 		{
-			reloadFile << line << std::endl;
+			if (std::stoi(line) > 0)
+			 reloadFile << std::endl << line;
+			else
+			{
+				reloadFile << line << std::endl;
+			}
 		}
 		questionBackup.close();
 		reloadFile.close();
@@ -42,6 +47,7 @@ void QuizInput::questionHandler()
 	std::string deleteLine;
 	std::vector<std::string> inSessionLines;
 	int random = 0;
+	int randomEntry = 0;
 	finish = false;
 
 	while (getline(populateFile, line))
@@ -50,21 +56,28 @@ void QuizInput::questionHandler()
 		inSessionLines.push_back(line);
 	}
 
-	if (removalFile.eof())
-	{
-		finish = true;
-		return;
-	}
-
 	if (!removalFile.eof())
 	{
 		bool populated = true;
-		random = rand() % lineNum;
+		if (inSessionLines.size() == 0)
+		{
+			finish = true;
+			return;
+		}
+		else
+		{
+			random = rand() % inSessionLines.size();
+		}
+
+		randomEntry = std::stoi(inSessionLines[random]);
 		if (populated)
-			if (random == 0)
-				random = rand() % lineNum;
+			if (randomEntry == 0)
+			{
+				random = rand() % inSessionLines.size();
+				randomEntry = std::stoi(inSessionLines[random]);
+			}
 	}
-	Q_num = random;
+	Q_num = randomEntry;
 	populateFile.clear();
 	populateFile.seekg(0);
 	lineNum = 0;
@@ -85,33 +98,32 @@ void QuizInput::questionHandler()
 	removalFile.close();
 	std::remove("../Resources/Quiz/QuizPopulate.txt");
 	std::rename("../Resources/Quiz/QuizPopulateInSession.txt", "../Resources/Quiz/QuizPopulate.txt");
-	if (random == 0)
-	{
-		finish = true;
-		return;
-	}
 }
 
 void QuizInput::handleInput(char input, AbstractQuestion** question)
 {
-	questionHandler();
-	if (finish)
-	{
-		system("CLS");
-		std::cout << "You have finished the assessment.\n\n"
-			"Your score is: " << player->getScore(); 
-		std::cout << std::endl << std::endl;
-		system("PAUSE");
-		player->reset();
-		*question = new Menu(player);
-	}
 	AbstractQuestion* q = *question;
+	questionHandler();
 	switch (input)
 	{
 	case ('1'):
 		system("CLS");
-		*question = new Quiz(player, Q_num);
-		delete q;
+		if (finish)
+		{
+			system("CLS");
+			std::cout << "You have finished the assessment.\n\n"
+				"Your score is: " << player->getScore();
+			std::cout << std::endl << std::endl;
+			system("PAUSE");
+			player->reset();
+			*question = new Menu(player);
+			delete q;
+		}
+		else
+		{
+			*question = new Quiz(player, Q_num);
+			delete q;
+		}
 		break;
 	case ('2'):
 		system("CLS");
